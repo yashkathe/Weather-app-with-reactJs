5df75cb9340b30a97e991338c33db37af049d492
Yash Kathe <yashkathe123@gmail.com>
2021-12-18 14:03:18 +0530

    optimised styles for smaller devices

--- src/Components/StartingForm.js
+++ src/Components/StartingForm.js
@@ -1,24 +1,32 @@
 import React from "react";
 
 import styles from "./MainForm.module.css";
-import {motion} from 'framer-motion';
+import { motion } from "framer-motion";
 
 const StartingForm = (props) => {
     return (
         <React.Fragment>
+            <div className={styles.blur}></div>
             <div className={styles.main}>
                 <form className={styles.content} onSubmit={props.handler}>
-                    <input placeholder='Your city' ref={props.cityref} className={styles.mainInput}/>
+                    <input
+                        placeholder='Your city'
+                        ref={props.cityref}
+                        className={styles.mainInput}
+                    />
                 </form>
                 <div className={styles.content}>
-                    <h1 className={styles.Name}>Enter the name of your city</h1>
-                    <motion.p className={styles.descript} animate={{
-                        scale: [1.1,1,1.1,1,1.1],
-                        transition:{
-                            duration:10,
-                            repeat:Infinity
-                        }
-                    }}>
+                    <h1 className={styles.Name2}>Enter the name of your city</h1>
+                    <motion.p
+                        className={styles.descript}
+                        animate={{
+                            scale: [1.1, 1, 1.1, 1, 1.1],
+                            transition: {
+                                duration: 10,
+                                repeat: Infinity,
+                            },
+                        }}
+                    >
                         Powered by - open weather api
                     </motion.p>
                 </div>
