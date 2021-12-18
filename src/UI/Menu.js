3841aaee90bfb57c99f3edd8ea13c2f3dfea0796
Yash Kathe <yashkathe123@gmail.com>
2021-12-18 12:34:53 +0530

    hamburger menu files added

---
+++ src/UI/Menu.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+import classes from "./Menu.module.css";
+
+import { NavLink } from "react-router-dom";
+
+const Menu = () => {
+    const [clicked, setIsClicked] = useState(false);
+
+    const HmenuHandler = () => {
+        setIsClicked(!clicked);
+    };
+
+    const navBar = clicked === false ? classes.navBar : classes.navBarClicked
+    const backDrop = clicked === false ? classes.backDrop : classes.backDropClicked
+
+    return (
+        <React.Fragment>
+            <div onClick={HmenuHandler}>
+                <div className={classes.HMenu}></div>
+                <div className={classes.HMenu}></div>
+                <div className={classes.HMenu}></div>
+            </div>
+            <div className={navBar}>
+                <NavLink to='/' activeClassName={classes.active} exact>
+                    <div>
+                        <button>Home</button>
+                    </div>
+                </NavLink>
+                <a
+                    href='https://github.com/yashkathe/Weather-app-with-reactJs/blob/master/src/App.css'
+                    rel='noreferrer noopener'
+                    target='_blank'
+                >
+                    Source Code
+                </a>
+                <NavLink to='/settings' activeClassName={classes.active}>
+                    <div>
+                        <button>Settings</button>
+                    </div>
+                </NavLink>
+            </div>
+            <div className={backDrop} onClick={HmenuHandler}></div>
+        </React.Fragment>
+    );
+};
+
+export default Menu;
