6d68bc2e9cf70cb1b4cec66197556e08925512b9
Yash Kathe <yashkathe123@gmail.com>
2022-02-22 13:29:26 +0530

    Modal animations added

--- src/UI/Menu.js
+++ src/UI/Menu.js
@@ -3,44 +3,96 @@ import React, { useState } from "react";
 import classes from "./Menu.module.css";
 
 import { NavLink } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Menu = () => {
-    const [clicked, setIsClicked] = useState(false);
+    // const [clicked, setIsClicked] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
     const HmenuHandler = () => {
-        setIsClicked(!clicked);
+        setShowModal(!showModal);
     };
 
-    const navBar = clicked === false ? classes.navBar : classes.navBarClicked
-    const backDrop = clicked === false ? classes.backDrop : classes.backDropClicked
+    const navBarVarients = {
+        initial: {
+            x: "-70vw",
+            transition: {
+                duration: 1,
+            },
+        },
+        animate: {
+            x: "0vw",
+            transition: {
+                duration: 1,
+            },
+        },
+    };
+
+    const backDropVar = {
+        initial: {
+            opacity: 0,
+            transition: {
+                duration: 1,
+            },
+        },
+        animate: {
+            opacity: 1,
+            transition: {
+                duration: 1,
+            },
+        },
+    };
 
     return (
         <React.Fragment>
             <div onClick={HmenuHandler}>
-                <div className={classes.HMenu}></div>
-                <div className={classes.HMenu}></div>
-                <div className={classes.HMenu}></div>
+                <motion.div className={classes.HMenu}></motion.div>
+                <motion.div className={classes.HMenu}></motion.div>
+                <motion.div className={classes.HMenu}></motion.div>
             </div>
-            <div className={navBar}>
-                <NavLink to='/' activeClassName={classes.active} exact>
-                    <div>
-                        <button>Home</button>
-                    </div>
-                </NavLink>
-                <a
-                    href='https://github.com/yashkathe/Weather-app-with-reactJs'
-                    rel='noreferrer noopener'
-                    target='_blank'
-                >
-                    Source Code
-                </a>
-                <NavLink to='/settings' activeClassName={classes.active}>
-                    <div>
-                        <button>Settings</button>
-                    </div>
-                </NavLink>
-            </div>
-            <div className={backDrop} onClick={HmenuHandler}></div>
+
+            <AnimatePresence>
+                {showModal && (
+                    <motion.div
+                        className={classes.navBar}
+                        variants={navBarVarients}
+                        initial='initial'
+                        animate='animate'
+                        exit='initial'
+                    >  
+                        <NavLink to='/' activeClassName={classes.active} exact>
+                            <button>Home</button>
+                        </NavLink>
+                        <NavLink
+                            to='/settings'
+                            activeClassName={classes.active}
+                        >
+                            <button>Settings</button>
+                        </NavLink>
+                        <a
+                            href='https://github.com/yashkathe/Weather-app-with-reactJs'
+                            rel='noreferrer noopener'
+                            target='_blank'
+                        >
+                            Source Code
+                        </a>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+
+            {/* backdrop  */}
+            <AnimatePresence>
+                {showModal === true && (
+                    <motion.div
+                        className={classes.backDrop}
+                        onClick={HmenuHandler}
+                        variants={backDropVar}
+                        initial='initial'
+                        animate='animate'
+                        exit='initial'
+                    ></motion.div>
+                )}
+            </AnimatePresence>
         </React.Fragment>
     );
 };
